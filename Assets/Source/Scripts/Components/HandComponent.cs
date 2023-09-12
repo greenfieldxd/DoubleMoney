f26be6f97a74cc341011a6d4f5358e0278a6be08
greenfieldxd <monsterbig53@gmail.com>
2023-09-12 21:35:28 +0300

    Create new anims

---
+++ Assets/Source/Scripts/Components/HandComponent.cs
@@ -0,0 +1,11 @@
+﻿using Source.Scripts.Enums;
+using UnityEngine;
+
+namespace Source.Scripts.Components
+{
+    public class HandComponent : MonoBehaviour
+    {
+        [field:SerializeField] public TurnType TurnType { get; private set; }
+        [field:SerializeField] public Animator Animator { get; private set; }
+    }
+}
---
+++ Assets/Source/Scripts/Systems/Game/HandMoveSystem.cs
@@ -0,0 +1,12 @@
+﻿using Kuhpik;
+
+namespace Source.Scripts.Systems.Game
+{
+    public class HandMoveSystem : GameSystem
+    {
+        public override void OnInit()
+        {
+            
+        }
+    }
+}
